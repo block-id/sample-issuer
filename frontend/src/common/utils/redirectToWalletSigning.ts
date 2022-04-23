32d0137d107cf07bbe19d8a79c049d95d96f5c72
areebbeigh <areebbeigh@gmail.com>
2022-04-23 22:38:54 +0530

    frontend.redirectToWallet: Fix localhost redirects

--- frontend/src/common/utils/redirectToWalletSigning.ts
+++ frontend/src/common/utils/redirectToWalletSigning.ts
@@ -2,7 +2,13 @@ import { WALLET_SIGNING_URL } from 'common/utils/constants';
 
 function redirectToWalletSigning(payload: string, challengeId: number): void {
   const { host } = window.location;
-  const redirectUrl = `http://${host}/sign-challenge/${challengeId}/`;
+  // We can't use the same host (in case of local) since cookies will be overwritten
+  const hostMap: {[key: string]: string} = {
+    localhost: '127.0.0.1',
+    '127.0.0.1': 'localhost',
+  };
+  const newHost = hostMap[host] || '127.0.0.1';
+  const redirectUrl = `http://${newHost}/sign-challenge/${challengeId}/`;
   window.location.href = `${WALLET_SIGNING_URL}?payload=${payload}&redirect=${redirectUrl}`;
 }
 
